00a2b3fc0a0e7f7d0e97ae794b1a3b96a30f81e0
Gianma23 <gianmaria.saggini@gmail.com>
2022-11-01 23:23:58 +0100

    first class

---
+++ src/main/java/it/unipi/chess/Chess.java
@@ -0,0 +1,13 @@
+package it.unipi.chess;
+
+/**
+ *
+ * @author Utente
+ */
+public class Chess {
+    
+    public static void main(String[] args) {
+                System.out.println("Hello World!");
+                System.out.println("Hello World!");
+    }
+}
